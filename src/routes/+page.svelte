1c544fa60949aff9775fb9e7097372767c228f64
Tanaka <alongthejourneyahead@gmail.com>
2023-06-08 00:14:07 +0100

    fix the alignment issue

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,160 +1,166 @@
 <script>
-    let query = "";
-    let waitingOnResponse = false;
-    let responseText = "";
-    let q = "";
+  import Box from './Box.svelte';
+
+let query = "";
+let waitingOnResponse = false;
+let responseText = "";
+let q = "";
 
 
-  
-    async function sendRequest() {
-      q = query
 
-      const url = "http://localhost:8080"; // Replace with your server URL
-      const payload = { query }; 
-  
-      waitingOnResponse = true 
-      try {
-        const response = await fetch(url, {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify(payload),
-        });
+async function sendRequest() {
+  q = query
 
-       
-  
-        if (response.ok) {
-          const data = await response.json();
-          responseText = data.result;
-          waitingOnResponse = false
-        } else {
-          responseText = "Error: " + response.status;
-        }
-      } catch (error) {
-        responseText = "Error: " + error.message;
-      }
+  const url = "http://localhost:8080"; // Replace with your server URL
+  const payload = { query }; 
 
-      query = ""
-      
+  waitingOnResponse = true 
+  try {
+    const response = await fetch(url, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(payload),
+    });
+
+    
+
+    if (response.ok) {
+      const data = await response.json();
+      responseText = data.result;
+      waitingOnResponse = false
+    } else {
+      responseText = "Error: " + response.status;
     }
+  } catch (error) {
+    responseText = "Error: " + error.message;
+  }
+
+  query = ""
+  
+}
 
 
-    function handleInput(event) {
-      event.target.style.height = 'auto';
-      event.target.style.height = `${event.target.scrollHeight}px`;
-    }
 </script>
 
-  
+
 <style lang="postcss">
 
 :global(html) {
-        background-color: theme(colors.gray.100);
+  background-color: theme(colors.white);
+  margin: 0; padding: 0; 
 }
 
 .input-container {
-  display: flex;
-  flex-direction: column;
-  align-items: flex-start; /* Align items to the start of the container */
-  justify-content: flex-end; /* Align items to the bottom of the container */
+display: flex;
+flex-direction: column;
+align-items: flex-start; /* Align items to the start of the container */
+justify-content: flex-end; /* Align items to the bottom of the container */
 }
 
 .input {
-  width: 95%;
-  border: 1px solid #ccc;
-  padding: 10px;
-  font-size: 20px;
-  resize: vertical;
-  min-height: 200px;
-  margin-bottom: 10px;
+width: 97%;
+//width: auto;
+border: 1px solid #ccc;
+padding: 10px;
+font-size: 20px;
+resize: vertical;
+min-height: 200px;
+margin-bottom: 10px;
+border-radius: 5px;
+
+resize: none; /* Prevent manual resizing */
+overflow: hidden; /* Hide scrollbars */
 }
 
 .button {
-  padding: 10px 20px;
-  font-size: 16px;
-  font-weight: bold;
-  text-align: center;
-  text-decoration: none;
-  text-transform: uppercase;
-  border-radius: 4px;
-  border: 2px solid rgba(76, 175, 80, 0.5);
-  color: #fff;
-  background-color: #4CAF50;
-  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.2);
-  transition: color 0.3s ease, background-color 0.3s ease;
-  cursor: pointer;
-  width: auto; /* Set width to auto to occupy only the necessary space */
-  align-self: flex-end; /* Align the button to the right within the container */
-}
-
-
-
-.input_ {
-      resize: none; /* Prevent manual resizing */
-      overflow: hidden; /* Hide scrollbars */
+padding: 10px 20px;
+font-size: 16px;
+font-weight: bold;
+text-align: center;
+text-decoration: none;
+text-transform: uppercase;
+border-radius: 4px;
+border: 2px solid rgba(76, 175, 80, 0.5);
+color: #fff;
+background-color: #4CAF50;
+box-shadow: 0 2px 4px rgba(0, 0, 0, 0.2);
+transition: color 0.3s ease, background-color 0.3s ease;
+cursor: pointer;
+width: auto; /* Set width to auto to occupy only the necessary space */
+align-self: flex-end; /* Align the button to the right within the container */
 }
 
 .button:hover {
-    background-color: #75ca79;
+background-color: #75ca79;
 }
 
-  .button:active {
-    background-color: #3e8e41;
-  }
+.button:active {
+background-color: #3e8e41;
+}
 
-  .button:focus {
-    outline: none;
-  }
+.button:focus {
+outline: none;
+}
 
-  
-  .answer {
-    font-size: 20px;
-  }
 
-  .features {
-    font-weight: 600;
-    font-size: 32px;    
-  }
+.answer {
+font-size: 20px;
+}
 
-  .container {
-      max-width: 800px;
-      margin: 0 auto;
-      padding: 20px;
-  }
+.features {
+font-weight: 600;
+font-size: 32px;    
+}
+
+.container {
+  width: 60%;
+  margin: auto;
+}
 
 </style>
-  
+
 
 <main>
-  <div class="container">
-    <div class="features">Your gardening helper</div>
 
-    {#if !responseText}
-      <h2>I answer all gardening questions</h2>
 
-      <div class="input-container">
-        <textarea class="input input_" type="text" bind:value={query} on:input={query}></textarea>
-        <button class="button" on:click={sendRequest}>Send</button>
-      </div>
-    {/if}
 
-    {#if waitingOnResponse}
-      <p>Waiting for response</p>
-    {/if}
 
-    {#if responseText}
-      <div>
-        <h2>Question</h2>
-        <p>{q}</p>
-        <h2>Answer</h2>
-        <p class="answer">{responseText}</p>
-      </div>
 
-      <div class="input-container">
-        <textarea class="input input_" type="text" bind:value={query} on:input={query}></textarea>
-        <button class="button" on:click={sendRequest}>Send</button>
-      </div>
-    {/if}
+<div class="container">
+<Box>
+  <p>This App uses AI models which may produce inaccurate answers that could be unfactual or harmful. It is limited to only answer questions about gardening or related topics</p>
+</Box>
+
+
+<div class="features">Your gardening assistant</div>
+
+{#if !responseText}
+  <h3>I answer all gardening questions</h3>
+
+  <div class="input-container">
+    <textarea class="input" type="text" bind:value={query} on:input={query}></textarea>
+    <button class="button" on:click={sendRequest}>Send</button>
   </div>
+{/if}
+
+{#if waitingOnResponse}
+  <p>Waiting for response</p>
+{/if}
+
+{#if responseText}
+  <div>
+    <h2>Question</h2>
+    <p>{q}</p>
+    <h2>Answer</h2>
+    <p class="answer">{responseText}</p>
+  </div>
+
+  <div class="input-container">
+    <textarea class="input input_" type="text" bind:value={query} on:input={query}></textarea>
+    <button class="button" on:click={sendRequest}>Send</button>
+  </div>
+{/if}
+</div>
 </main>
